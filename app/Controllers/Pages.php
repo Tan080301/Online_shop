07671ae376b73b0948015255c60fd8fb0953f9ec
Tan080301 <72324275+Tan080301@users.noreply.github.com>
2020-12-23 20:24:02 +0700

    Update Pages.php

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -17,12 +17,32 @@ class Pages extends BaseController
 
         $data = [
             'title' => 'Home | Otlizshop',
-            'db_barang' => $db_barang
+            'db_barang' => $this->dataBase->getPages()
         ];
 
         return view('/pages/home', $data);
     }
 
+    public function features()
+
+    {
+        $data = [
+            'title' => 'Features | Otlizshop'
+        ];
+
+        return view('/pages/features', $data);
+    }
+
+
+    public function detail($slug)
+    {
+        $data = [
+            'title' => 'Detail Produk',
+            'db_barang' => $this->dataBase->getPages($slug)
+        ];
+
+        return view('/databarang/detail', $data);
+    }
     //--------------------------------------------------------------------
 
 }
